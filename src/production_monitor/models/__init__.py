f2e915a8203f732947e9a40d68e3a5140d39eb9d
Pasha Ryaguzov <pashajobber@gmail.com>
2025-05-13 18:11:08 +0500

    dev: add db

---
+++ src/production_monitor/models/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ("Base", "ProductionBatch")
+
+from src.production_monitor.models.base import Base
+from src.production_monitor.models.production import ProductionBatch
---
+++ src/production_monitor/utils/case_converter.py
@@ -0,0 +1,27 @@
+def camel_case_to_snake_case(input_str: str) -> str:
+    """
+    >>> camel_case_to_snake_case("SomeSDK")
+    'some_sdk'
+    >>> camel_case_to_snake_case("RServoDrive")
+    'r_servo_drive'
+    >>> camel_case_to_snake_case("SDKDemo")
+    'sdk_demo'
+    >>> camel_case_to_snake_case("VacancyY")
+    'vacanc_ies'
+    """
+    chars = []
+    for c_idx, char in enumerate(input_str):
+        if c_idx and char.isupper():
+            nxt_idx = c_idx + 1
+            flag = nxt_idx >= len(input_str) or input_str[nxt_idx].isupper()
+            prev_char = input_str[c_idx - 1]
+            if prev_char.isupper() and flag:
+                pass
+            else:
+                chars.append("_")
+        chars.append(char.lower())
+
+    if chars[-1] == "y":
+        chars = chars[:-1] + ["i", "e"]
+
+    return "".join(chars)
